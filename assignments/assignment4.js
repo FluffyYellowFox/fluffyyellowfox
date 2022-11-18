00c0818629159c9869030468d777e6b029ae6e7f
FluffyYellowFox <113108714+FluffyYellowFox@users.noreply.github.com>
2022-11-17 22:27:23 -0500

    Create assignment4.js

---
+++ assignments/assignment4.js
@@ -0,0 +1,14 @@
+var Arr1 = [(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100)];
+var Arr2 = [(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200)];
+var Arr3 = [
+  {fname: "John", lname: "Adams", title: "President", suffix: "elect", age: 225},
+  {fname: "Joseph", lname: "Johnson", title: "President", suffix: "elect", age: 117},
+  {fname: "Joe", lname: "James", title: "President", suffix: "elect", age: 186},
+  {fname: "Bob", lname: "Jones", title: "President", suffix: "elect", age: 214},
+  {fname: "Sam", lname: "Adams", title: "President", suffix: "elect", age: 197}
+  ];
+let years = [-100, 0, 100, 1900, 1904, 2000];
+let r = [1, 4, 5, 7];
+let distances = [2, 4, 6, 8, 10];
+
+let pyth = (a,b) => a*a+b*b;
