2c65793c06fe575e1e59403757747541b59dc387
FluffyYellowFox <113108714+FluffyYellowFox@users.noreply.github.com>
2022-11-25 16:53:14 -0500

    Update assignment4.js

--- assignments/assignment4.js
+++ assignments/assignment4.js
@@ -1,17 +1,3 @@
-var Arr1 = [(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100)];
-var Arr2 = [(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200)];
-var Arr3 = [
-  {fname: "John", lname: "Adams", title: "President", suffix: "elect", age: 225},
-  {fname: "Joseph", lname: "Johnson", title: "President", suffix: "elect", age: 117},
-  {fname: "Joe", lname: "James", title: "President", suffix: "elect", age: 186},
-  {fname: "Bob", lname: "Jones", title: "President", suffix: "elect", age: 214},
-  {fname: "Sam", lname: "Adams", title: "President", suffix: "elect", age: 197}
-  ];
-let years = [-100, 0, 100, 1900, 1904, 2000];
-let r = [1, 4, 5, 7];
-let distances = [2, 4, 6, 8, 10];
-
-let pyth = (a,b) => a*a+b*b;
 //Arrays
 var Arr1 = [(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100),(Math.floor(Math.random()*1000)+100)];
 var Arr2 = [(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200),(Math.floor(Math.random()*400)-200)];
@@ -48,3 +34,52 @@ let sumEven = Arr2.filter(e => e%2==0).reduce((accumulator, n) => accumulator + 
 let fourVol = Arr5.map(e => sphrad(e));
 let fallTimes = Arr6.map(e => timefallen(e));
 let arcsum = Arr5.map(e => arctan(Math.PI,e)).reduce((accumulator, n) => accumulator + n);
+
+//Part 2
+function loadData() {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      covidData = this.responseText
+      document.getElementById("demo").innerHTML = covidData;
+      covidData = JSON.parse(covidData);
+      totalID =
+        covidData.Global.ID;
+      totalCountry =
+        covidData.Global.Country;
+      totalCountryCode =
+        covidData.Global.CountryCode;
+      totalSlug =
+        covidData.Global.Slug;
+      totalNewConfirmed =
+        covidData.Global.NewConfirmed;
+      totalConfirmed = 
+        covidData.Global.TotalConfirmed;
+      totalNewDeaths =
+        covidData.Global.NewDeaths;
+      totalDeaths =  
+        covidData.Global.TotalDeaths;
+      totalNewRecovered =
+        covidData.Global.NewRecovered;
+      totalRecovered =  
+        covidData.Global.TotalRecovered;
+      totalDate =
+        covidData.Global.Date;
+    }
+  };
+  let url = "https://api.covid19api.com/summary";
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+loadData();
+
+let counter = 0;
+
+function previous () {
+  if (counter > 0) {
+    counter -=1;
+  }
+}
+function next () {
+    counter +=1;
+}
